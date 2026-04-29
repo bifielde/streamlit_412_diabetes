75dc474c371d108b11e9216a7d2287da7a0c8863
bifielde <bifielde@asu.edu>
2026-04-29 04:17:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ high_threshold = .35
 med_threshold = .25
 
 form = st.form(key = "input")
-form.header("Enter information below")
+form.header("Diabetes risk screening form - Please enter your information below")
 form.text('For checkbox questions, check the associated box if the answer is YES. Otherwise, leave the box blank. Please read all questions carefully and answer to the best of your knowledge.')
 high_BP = form.checkbox('Have you been diagnosed with high blood pressure?')
 if high_BP:
